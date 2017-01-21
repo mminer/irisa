10a3f42060cf5df6278def53c1d62595a88d4b1a
Matthew Miner <matthew@matthewminer.com>
2017-01-20 17:42:16 -0800

    Implement key press handler.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import './style.css';
-console.log('Hello World');
+import { DOWN, LEFT, RIGHT, UP } from 'key-codes';
+
+const keyHandlers = {
+  [LEFT]: () => console.log('left'),
+  [UP]: () => console.log('up'),
+  [RIGHT]: () => console.log('right'),
+  [DOWN]: () => console.log('down'),
+};
+
+const keysCurrentlyPressed = new Set();
+
+document.addEventListener('keydown', evt => {
+  const { keyCode } = evt;
+
+  // Prevent keys from repeating.
+  if (keysCurrentlyPressed.has(keyCode)) {
+    return;
+  }
+
+  const handler = keyHandlers[keyCode];
+
+  // Ignore keys that we lack a handler for.
+  if (!handler) {
+    return;
+  }
+
+  handler();
+  keysCurrentlyPressed.add(keyCode);
+
+  evt.preventDefault();
+  evt.stopPropagation();
+});
+
+document.addEventListener('keyup', evt => {
+  keysCurrentlyPressed.delete(evt.keyCode);
+});
