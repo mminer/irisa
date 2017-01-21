0d9f7cb0ec1e26060eca0e430323867d434f17b0
Matthew Miner <matthew@matthewminer.com>
2017-01-20 17:32:54 -0800

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const cssnext = require('postcss-cssnext');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+        },
+        test: /\.js$/,
+      },
+      {
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader',
+        ],
+        test: /\.css$/,
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: 'static',
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: () => [cssnext],
+      },
+    }),
+  ],
+  resolve: {
+    modules: ['node_modules', 'src'],
+  },
+};
